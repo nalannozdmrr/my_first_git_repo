f3fb853d4971d4c5e8fcd639bf96fb4df36f1e64
Na35263526 <nalan.ozdemir2002@gmail.com>
2023-04-08 13:29:45 +0300

    third commit

--- src/First_JavaClass.java
+++ src/First_JavaClass.java
@@ -1,5 +1,7 @@
 public class First_JavaClass {
     public static void main(String[] args) {
         System.out.println("Learning github");
+        System.out.println("Adding a new code");
+        System.out.println("This is an other code");
     }
 }
